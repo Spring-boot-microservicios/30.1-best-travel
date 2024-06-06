3cdce326bdeeda48754cf169af7908886e3e202f
Luis Angel <angelfgdeveloper@gmail.com>
2024-06-06 02:13:36 -0600

    Uso de JPQL

--- src/main/java/com/angelfg/best_travel/BestTravelApplication.java
+++ src/main/java/com/angelfg/best_travel/BestTravelApplication.java
@@ -4,11 +4,11 @@ import com.angelfg.best_travel.domain.entities.*;
 import com.angelfg.best_travel.domain.repositories.jpa.*;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.math.BigDecimal;
 import java.util.UUID;
 
 @SpringBootApplication
@@ -29,17 +29,28 @@ public class BestTravelApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		FlyEntity fly = flyRepository.findById(15L).get();
-		HotelEntity hotel = hotelRepository.findById(7L).get();
-		TicketEntity ticket = ticketRepository.findById(UUID.fromString("22345678-1234-5678-3235-567812345678")).get();
-		ReservationEntity reservation = reservationRepository.findById(UUID.fromString("32345678-1234-5678-1234-567812345678")).get();
-		CustomerEntity customer= customerRepository.findById("BBMB771012HMCRR022").get();
+//		FlyEntity fly = flyRepository.findById(15L).get();
+//		HotelEntity hotel = hotelRepository.findById(7L).get();
+//		TicketEntity ticket = ticketRepository.findById(UUID.fromString("22345678-1234-5678-3235-567812345678")).get();
+//		ReservationEntity reservation = reservationRepository.findById(UUID.fromString("32345678-1234-5678-1234-567812345678")).get();
+//		CustomerEntity customer= customerRepository.findById("BBMB771012HMCRR022").get();
+//
+//		log.info(String.valueOf(fly));
+//		log.info(String.valueOf(hotel));
+//		log.info(String.valueOf(ticket));
+//		log.info(String.valueOf(reservation));
+//		log.info(String.valueOf(customer));
 
-		log.info(String.valueOf(fly));
-		log.info(String.valueOf(hotel));
-		log.info(String.valueOf(ticket));
-		log.info(String.valueOf(reservation));
-		log.info(String.valueOf(customer));
+		this.flyRepository.selectLessPrice(BigDecimal.valueOf(20)).forEach(System.out::println);
+
+		System.out.println("---------------------------");
+
+		this.flyRepository.selectBetweenPrice(BigDecimal.valueOf(10), BigDecimal.valueOf(15)).forEach(System.out::println);
+
+		System.out.println("---------------------------");
+
+		this.flyRepository.selectOriginDestiny("Grecia", "Mexico").forEach(System.out::println);
+
 	}
 
 }
--- src/main/java/com/angelfg/best_travel/domain/repositories/jpa/FlyRepository.java
+++ src/main/java/com/angelfg/best_travel/domain/repositories/jpa/FlyRepository.java
@@ -2,7 +2,21 @@ package com.angelfg.best_travel.domain.repositories.jpa;
 
 import com.angelfg.best_travel.domain.entities.FlyEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.math.BigDecimal;
+import java.util.Set;
 
 public interface FlyRepository extends JpaRepository<FlyEntity, Long> {
+
+    // JPQL
+    @Query(value = "select f from fly f where f.price < :price")
+    Set<FlyEntity> selectLessPrice(BigDecimal price);
+
+    @Query(value = "select f from fly f where f.price between :min and :max")
+    Set<FlyEntity> selectBetweenPrice(BigDecimal min, BigDecimal max);
+
+    @Query(value = "select f from fly f where f.originName = :origin and f.destinyName = :destiny")
+    Set<FlyEntity> selectOriginDestiny(String origin, String destiny);
 
 }
