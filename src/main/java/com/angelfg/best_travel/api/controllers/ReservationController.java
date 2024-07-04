fe66f26653752aed1d7ab9620201b214ca58ea0f
Luis Angel <angelfgdeveloper@gmail.com>
2024-07-04 02:33:28 -0600

    Uso de validations

--- src/main/java/com/angelfg/best_travel/api/controllers/ReservationController.java
+++ src/main/java/com/angelfg/best_travel/api/controllers/ReservationController.java
@@ -3,6 +3,7 @@ package com.angelfg.best_travel.api.controllers;
 import com.angelfg.best_travel.api.dtos.request.ReservationRequest;
 import com.angelfg.best_travel.api.dtos.response.ReservationResponse;
 import com.angelfg.best_travel.infraestructure.abstract_services.ReservationService;
+import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -20,7 +21,7 @@ public class ReservationController {
     private final ReservationService reservationService;
 
     @PostMapping
-    public ResponseEntity<ReservationResponse> post(@RequestBody ReservationRequest request) {
+    public ResponseEntity<ReservationResponse> post(@Valid @RequestBody ReservationRequest request) {
         return ResponseEntity.ok(this.reservationService.create(request));
     }
 
@@ -30,7 +31,7 @@ public class ReservationController {
     }
 
     @PutMapping(path = "{id}")
-    public ResponseEntity<ReservationResponse> put(@PathVariable UUID id, @RequestBody ReservationRequest request) {
+    public ResponseEntity<ReservationResponse> put(@Valid @PathVariable UUID id, @RequestBody ReservationRequest request) {
         return ResponseEntity.ok(this.reservationService.update(request, id));
     }
 
--- src/main/java/com/angelfg/best_travel/api/dtos/request/ReservationRequest.java
+++ src/main/java/com/angelfg/best_travel/api/dtos/request/ReservationRequest.java
@@ -1,5 +1,6 @@
 package com.angelfg.best_travel.api.dtos.request;
 
+import jakarta.validation.constraints.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -12,7 +13,22 @@ import java.io.Serializable;
 @NoArgsConstructor
 @Builder
 public class ReservationRequest implements Serializable {
+
+    // valida 18 y 20 caracteres en strings
+    @Size(min = 18, max = 20, message = "The size have to a length between 18 and 20 characters")
+    @NotBlank(message = "Id client is mandatory") // no puede venir vacio ni null
     private String idClient;
+
+    @Positive // Deben ser numeros positivos
+    @NotNull(message = "Id hotel is mandatory")
     private Long idHotel;
+
+    @Min(value =  1, message = "Min one days to make reservation")
+    @Max(value = 30, message = "Max 30 days to make reservation")
+    @NotNull(message = "total days is mandatory")
     private Integer totalDays;
+
+    // @Pattern(regexp = "^(.+)@(.+)$") // expresion regular para verificar un email
+    @Email(message = "Invalid email")
+    private String email;
 }
