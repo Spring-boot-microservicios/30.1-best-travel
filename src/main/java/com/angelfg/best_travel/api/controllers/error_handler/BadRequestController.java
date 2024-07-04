1bb0c5b9461c3806bf1e64d232e22537290bb279
Luis Angel <angelfgdeveloper@gmail.com>
2024-07-04 02:00:57 -0600

    Generando un controller advice de excepcion

---
+++ src/main/java/com/angelfg/best_travel/api/controllers/error_handler/BadRequestController.java
@@ -0,0 +1,19 @@
+package com.angelfg.best_travel.api.controllers.error_handler;
+
+import com.angelfg.best_travel.util.exceptions.IdNotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+// 400, 401, ..
+@RestControllerAdvice
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+public class BadRequestController {
+
+    @ExceptionHandler(IdNotFoundException.class)
+    public String handleIdNotFound(IdNotFoundException exception) {
+        return exception.getMessage();
+    }
+
+}
